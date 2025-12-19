82d86ce689a0cc44124f97ac9101c7250fb85c92
baobab-boop <clende.com@gmail.com>
2025-12-19 18:42:07 +0500

    Add files via upload

---
+++ jobportal/urls.py
@@ -0,0 +1,13 @@
+﻿from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView, TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/vacancies/', permanent=False)),
+    path('accounts/', include('accounts.urls')),
+    path('vacancies/', include('vacancies.urls')),
+    path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
